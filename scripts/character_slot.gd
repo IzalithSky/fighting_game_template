59b67864a4859c038ad6e79c806363e188d500bf
Nadiia Ivanova <>
2024-10-12 15:24:11 +0100

    character slot - assign scene

--- scripts/character_slot.gd
+++ scripts/character_slot.gd
@@ -2,13 +2,25 @@
 class_name CharacterSlot
 extends Control
 
+var current_character_scene: PackedScene
+
 func _ready():
 	mouse_entered.connect(on_mouse_entered)
 	mouse_exited.connect(on_mouse_exited)
 	
 	focus_entered.connect(on_focus_entered)
 	focus_exited.connect(on_focus_exited)
-
+	
+func assign_character(character_data: Dictionary):
+	current_character_scene = load(character_data["scene_path"])
+	$NameLabel.text = character_data["name"]
+	
+	var character_instance = current_character_scene.instantiate()
+	var animated_sprite = character_instance.get_node("Animations") 
+	var sprite_frames: SpriteFrames = animated_sprite.sprite_frames
+	var frame_texture = sprite_frames.get_frame_texture("idle", 0)
+	$CharacterTextureRect.texture = frame_texture
+	
 func on_mouse_entered():
 	set_blinking(true)
 
--- scripts/player_selection.gd
+++ scripts/player_selection.gd
@@ -5,4 +5,7 @@ extends Control
 
 func _ready():
 	player1.grab_focus()
-	pass
+	
+	var characters = CharactersList.get_all_characters()
+	player1.assign_character(characters[0])
+	player2.assign_character(characters[1])
